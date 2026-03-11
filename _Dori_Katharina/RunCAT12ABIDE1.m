43879399f483893b1d49c2ccbe584e7def7da793
Arne Hansen <arne96.hansen@gmail.com>
2026-03-11 16:20:38 +0100

    add Dori template files MRI analysis

---
+++ _Dori_Katharina/RunCAT12ABIDE1.m
@@ -0,0 +1,29 @@
+s_cloud = 1;
+ 
+if s_cloud == 1
+    prefix = fullfile(filesep, 'mnt', 'methlab-drive','methlab');
+    jobfile = fullfile(prefix, 'Students','Katharina_Bremer','Scripts','Matlab', ...
+                       'SPM','VBM','Cat12OneSubjectServer.mat');
+else
+    prefix = fullfile(filesep, 'Volumes','G_PSYPLAFOR_methlab$');
+    jobfile = fullfile(prefix, 'Students','Katharina_Bremer','Scripts','Matlab', ...
+                       'SPM','VBM','Cat12OneSubject_jobFinal.mat');
+end
+
+load(jobfile, 'matlabbatch');
+
+addpath(fullfile(prefix, 'Students', 'Katharina_Bremer', 'Scripts', 'Matlab', 'matlab_installation', 'spm'));
+spm('defaults','PET');
+spm_jobman('initcfg');
+addpath(fullfile(prefix, 'Students', 'Katharina_Bremer', 'Scripts', 'Matlab', 'matlab_installation', 'spm','toolbox','cat12'));
+
+T = dir(fullfile(prefix,'Students/Katharina_Bremer/Input/ABIDE_I/*/mprage.nii'));
+T1_files = fullfile({T.folder}, {T.name});
+T1_files = strcat(T1_files, ',1');
+
+for i = 1:length(T1_files)
+    batch = matlabbatch;
+    batch{1}.spm.tools.cat.estwrite.data = {T1_files{i}};
+    spm_jobman('run', batch);
+end;
+
